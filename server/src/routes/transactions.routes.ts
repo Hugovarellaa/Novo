562597941de796a33f524602074c1b2bca87883f
hugo <hugovarellaa@gmail.com>
2021-03-13 11:35:05 -0300

    chore: create routes transaction (method: GET and POST)

---
+++ server/src/routes/transactions.routes.ts
@@ -0,0 +1,43 @@
+import { randomUUID } from 'crypto'
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { knex } from '../../database'
+
+export async function transactionsRoutes(app: FastifyInstance) {
+	app.post('/', async (request, reply) => {
+		const createTransactionSchema = z.object({
+			title: z.string(),
+			amount: z.number(),
+			category: z.string(),
+			type: z.enum(['income', 'outcome']),
+		})
+
+		const { title, amount, type, category } = createTransactionSchema.parse(
+			request.body,
+		)
+
+		let sessionId = request.cookies.sessionId
+
+		if (!sessionId) {
+			sessionId = request.cookies.sessionId
+		}
+
+		await knex('transactions')
+			.insert({
+				id: randomUUID(),
+				title,
+				amount,
+				type,
+				category,
+				session_id: sessionId,
+			})
+			.returning('*')
+
+		return reply.status(201).send()
+	})
+
+	app.get('/', async () => {
+		const transactions = await knex('transactions').select('*')
+		return { transactions }
+	})
+}
