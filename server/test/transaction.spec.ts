63d159262ebb90e39de7a9d9e69fcef29b558cc8
hugo <hugovarellaa@gmail.com>
2021-03-16 13:41:32 -0300

    chore: console.log cookie

--- server/test/transaction.spec.ts
+++ server/test/transaction.spec.ts
@@ -1,16 +1,47 @@
+import { execSync } from 'child_process'
 import request from 'supertest'
-import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import {
+	afterAll,
+	afterEach,
+	beforeAll,
+	beforeEach,
+	describe,
+	expect,
+	it,
+} from 'vitest'
 import { app } from '../src/app'
 
 describe('Transaction Routes', () => {
 	beforeAll(async () => {
-		app.ready()
-	})
-	afterAll(async () => {
-		app.close()
+		await app.ready()
 	})
 
-	it('should be able create a new transaction', async () => {
+	beforeEach(async () => {
+		execSync('npm run knex migrate:latest')
+	})
+
+	afterEach(async () => {
+		execSync('npm run knex migrate:rollback --all')
+	})
+
+	afterAll(async () => {
+		await app.close()
+	})
+
+	it.only('should be able create a new transaction', async () => {
+		const response = await request(app.server).post('/transactions').send({
+			title: 'Transaction test',
+			amount: 3000,
+			type: 'income',
+			category: 'Category Test',
+		})
+
+		console.log(response.headers['set-cookie'])
+
+		expect(response.statusCode).toBe(201)
+	})
+
+	it('should be able a list all transactions', async () => {
 		const createTransaction = await request(app.server)
 			.post('/transactions')
 			.send({
@@ -20,6 +51,7 @@ describe('Transaction Routes', () => {
 				category: 'Category Test',
 			})
 
-		expect(createTransaction.statusCode).toBe(201)
+		console.log(createTransaction.headers['set-cookie'])
+		// console.log(response.get('Set-Cookie')) -> forma de buscar os cookies
 	})
 })
