f2a64d2c5b0bbe2fa4c827dac9e97b6ffe043738
hugo <hugovarellaa@gmail.com>
2021-03-21 19:18:50 -0300

    chore: routes page

--- web/src/routes/app.routes.tsx
+++ web/src/routes/app.routes.tsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
+import { Layout } from '../layout'
 import { Home } from '../pages/Home'
 
 export function AppRoutes() {
 	return (
 		<Routes>
-			<Route path="/" element={<Home />} />
+			<Route path="/" element={<Layout />}>
+				<Route path="/" element={<Home />} />
+			</Route>
 		</Routes>
 	)
 }
