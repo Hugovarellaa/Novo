5f8b5304eb0f2bc85d0a928f7446972b0435c77d
hugo <hugovarellaa@gmail.com>
2021-03-25 21:07:28 -0300

    chore: component Summary

--- web/src/components/Summary/index.tsx
+++ web/src/components/Summary/index.tsx
@@ -1,9 +1,36 @@
+import {
+	ArrowCircleDown,
+	ArrowCircleUp,
+	CurrencyDollar,
+} from '@phosphor-icons/react'
 import { SummaryContainer } from './styles'
 
 export function Summary() {
 	return (
 		<SummaryContainer>
-			<h1>Summary</h1>
+			<div>
+				<header>
+					<span>Entradas</span>
+					<ArrowCircleUp size={32} />
+				</header>
+				<strong>R$ 17.400,00</strong>
+			</div>
+
+			<div>
+				<header>
+					<span>Entradas</span>
+					<ArrowCircleDown size={32} />
+				</header>
+				<strong>R$ 1.259,00</strong>
+			</div>
+
+			<div>
+				<header>
+					<span>Entradas</span>
+					<CurrencyDollar size={32} />
+				</header>
+				<strong>R$ 16.141,00</strong>
+			</div>
 		</SummaryContainer>
 	)
 }
