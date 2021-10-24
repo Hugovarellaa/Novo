0f5ccc2ef04fab638adf51f9de84f15a3f19cf33
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-10-23 22:14:26 -0300

    .
    .

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,21 +1,41 @@
 import axios from "axios";
 
+interface IAccessTokenResponse {
+  access_token: string;
+}
+
+interface IUserResponse {
+  avatar_url: string;
+  login: string;
+  id: number;
+  name: string;
+}
+
 class AuthenticateUserService {
   async execute(code: string) {
     const url = "https://github.com/login/oauth/access_token";
-    
-    const response = await axios.post(url, null, {
-      params: {
-        client_id: process.env.GITHUB_CLIENT_ID,
-        client_secret: process.env.GITHUB_CLIENT_SECRET,
-        code,
-      },
-      headers: {
-        "Accept": "application/json",
-      },
-    });
 
-    return response.data
+    const { data: access_tokenResponse } =
+      await axios.post<IAccessTokenResponse>(url, null, {
+        params: {
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code,
+        },
+        headers: {
+          Accept: "application/json",
+        },
+      });
+    const response = await axios.get<IUserResponse>(
+      "https://api.github.com/user",
+      {
+        headers: {
+          authorization: `bearer ${access_tokenResponse.access_token}`,
+        },
+      }
+    );
+
+    return response.data;
   }
 }
 
