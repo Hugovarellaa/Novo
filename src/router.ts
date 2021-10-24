382a03a22583113945d70271469b4aa0f839faad
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-10-24 18:03:58 -0300

    Finish

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateMessageController } from "./controllers/CreateMessageController";
 import { GetLast3MessagesController } from "./controllers/GetLast3MessagesController";
+import { ProfileUserController } from "./controllers/ProfileUserController";
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
@@ -13,4 +14,6 @@ router.post(
   new CreateMessageController().handle
 );
 router.get("/messages/last3", new GetLast3MessagesController().handle);
+router.get("/profile", ensureAuthenticated, new ProfileUserController().handle);
+
 export { router };
